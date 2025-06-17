ae3022ab3159f67b6c61c51ac5c98c03d7c1b96e
Evan Cortez <evanvcortez@gmail.com>
2025-06-16 21:00:12 -0600

    user endpoint

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from django.contrib.auth.models import User
 from .models import Project, Drawing, Component
 from .serializers import ProjectSerializer, DrawingSerializer, ComponentSerializer, UserSerializer
@@ -18,3 +20,10 @@ class ComponentViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user.id)
+    
+    @action(detail=False, methods=['get'])
+    def me(self, request):
+        return Response(self.get_serializer(request.user).data)
